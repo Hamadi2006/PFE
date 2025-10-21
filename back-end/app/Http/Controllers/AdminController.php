cfc2ccf64ea1781760019a0735edcea3a6b15833
aybhghggf <ayoubehamadi5@gmail.com>
2025-10-21 22:58:52 +0100

    front-end : admin authentifcation is done 1/2 v

--- back-end/app/Http/Controllers/AdminController.php
+++ back-end/app/Http/Controllers/AdminController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Admin;
+
 class AdminController extends Controller
 {
     public function login(Request $request)
@@ -19,15 +19,17 @@ class AdminController extends Controller
 
         if (! $user || ! Hash::check($request->mot_de_passe, $user->mot_de_passe)) {
             return response()->json([
-                'message' => 'Identifiants invalides'
+                'message' => 'Identifiants invalides',
             ], 401);
         }
 
         $token = $user->createToken('admin-token')->plainTextToken;
 
         return response()->json([
+            'message' => 'Authentification réussie',
             'token' => $token,
             'user' => $user,
-        ]);
+            'status' => 200,
+        ], 201); // code 201 = Created
     }
 }
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,62 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import {  Route, Routes} from 'react-router-dom';
+import { Route, Routes, useNavigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Footer from "./components/Footer";
 import Immobilier from "./Pages/Immobiler";
 import AdminLogin from "./components/admin/LoginPage";
-function App() {
+import axios from "axios";
+import Dash from "./components/admin/Dash";
 
-  function AuthAdmin(objet){
-    if(!objet ) return;
-    console.log("AuthAdmin information is",objet);
+function App() {
+  const navigate = useNavigate();
+
+  function AuthAdmin(objet) {
+    if (!objet) {
+      console.error("No authentication data provided");
+      return;
+    }
+
+    axios.post(
+      "http://127.0.0.1:8000/api/admin/login",
+      {
+        email: objet.email,
+        mot_de_passe: objet.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      console.log("Response from server:", response.data);
+
+      // test du code HTTP correct
+      if (response.status === 201) {
+        localStorage.setItem("admin_token", response.data.token);
+        localStorage.setItem(
+          "admin_name",
+          response.data.user.nom + " " + response.data.user.prenom
+        );
+        navigate("/backoffise/admin/adminPage/dashboard");
+      } else {
+        console.warn("Authentication failed with status:", response.status);
+      }
+    })
+    .catch((error) => {
+      console.error(
+        "There was an error during authentication:",
+        error.response?.data || error.message
+      );
+    });
   }
-  
-return (
+
+  return (
     <Routes>
-      {/* frontoffice*/}
+      {/* Frontoffice */}
       <Route
         path="/*"
         element={
@@ -24,16 +65,17 @@ return (
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/immobilier" element={<Immobilier />} />
+              <Route path="/backoffise/admin/adminPage/dashboard" element={<Dash />} />
             </Routes>
             <Footer />
           </>
         }
       />
 
-      {/* backoffice*/}
-        <Route
+      {/* Backoffice */}
+      <Route
         path="/backoffise/admin/adminPage/authentification"
-        element={<AdminLogin AuthAdmin={AuthAdmin}/>}
+        element={<AdminLogin AuthAdmin={AuthAdmin} />}
       />
     </Routes>
   );
